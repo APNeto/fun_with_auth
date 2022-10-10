d0bd9b2ae22609e400ff271531cf89f5f2d3e342
Alberto Pena <alberto.pena@voalle.com.br>
2022-10-09 22:39:35 -0300

    Added login (in memory)

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv'
 import bcrypt from 'bcrypt'
 dotenv.config()
 
-import express  from 'express'
+import express from 'express'
 const app = express()
 
 app.set('view-engine', 'ejs')
-app.use(express.urlencoded({extended: false }))
+app.use(express.urlencoded({ extended: false }))
 
 let users = []
 
@@ -18,7 +18,25 @@ app.get('/login', (req, res) => {
   res.render("login.ejs")
 })
 
+app.get('/home', (req, res) => {
+  res.render("home.ejs")
+})
+
 app.post('/login', (req, res) => {
+  if (!req.body.email) return res.status(400).json({ message: "Please send in an email" })
+
+  let user = users.find((user, index) => {
+
+    return user.email == req.body.email;
+  })
+  if (!user) return res.status(403).json({ message: "Not authenticated" })
+
+  bcrypt.compare(req.body.password, user.password, (err, _) => {
+    if (err) {
+      return res.status(403).json({ message: "Not authenticated" });
+    }
+    res.redirect("/home");
+  })
 })
 
 app.get('/register', (req, res) => {
@@ -27,7 +45,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
   bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if(err) {
+    if (err) {
       return res.status(500).json({ error: err })
     }
     else {
@@ -41,9 +59,9 @@ app.post('/register', (req, res) => {
   })
 })
 
-// app.get("/users", (req, res) => {
-//   res.send(users)
-// })
+app.get("/users", (req, res) => {
+  res.send(users)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on ${process.env.PORT}`)
